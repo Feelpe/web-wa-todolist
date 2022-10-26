9b20094275a5fcb2521a5ef047dfdde5cc2f4e62
felipe <felipesscreator@gmail.com>
2022-10-25 23:26:14 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import './header.scss'
+
+import logoWa from '../../assets/logo.svg'
+
+export function Header() {
+  return (
+    <header className="header">
+      <div>
+        <img src={logoWa} alt="WA logo"/>
+      </div>
+    </header>
+  )
+}
